2b3752eca81977ea602896248992e31f90aad705
Rob MacDonald <rob.m.macdonald@gmail.com>
2022-08-18 17:51:39 -0300

    Started overflow and underflow dates.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const date = new Date();
 const monthDays = document.querySelector(".days");
 const lastDay = new Date(date.getFullYear(), date.getMonth(), 0);
-console.log(lastDay);
+console.log(lastDay + "\n", lastDay.getDay());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.querySelector(".date h1").textContent = months[date.getMonth()];
 document.querySelector(".date p").textContent = date.toDateString();
 
 let days = "";
-for (let i = 0; i <= 31; i++) {
+for (let i = lastDay.getDay(); i >= 0; i--) {
+  days += `<div class="prev">${31 - i}</div>`;
+}
+
+for (let i = 1; i <= 31; i++) {
   days += `<div>${i}</div>`;
 }
+
+for (let i = 1; i <= 6; i++) {
+  days += `<div>${i}</div>`;
+}
+
 monthDays.innerHTML = days;
