24b252d9d84db6060bb40fc6e5c8dd319281e493
Rob-MacD <rob.m.macdonald@gmail.com>
2022-08-18 20:05:12 -0300

    more work on prev/post dates

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 const date = new Date();
+date.setDate(1);
 const monthDays = document.querySelector(".days");
-const lastDay = new Date(date.getFullYear(), date.getMonth(), 0);
-console.log(lastDay + "\n", lastDay.getDay());
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.querySelector(".date h1").textContent = months[date.getMonth()];
 document.querySelector(".date p").textContent = date.toDateString();
 
 let days = "";
-for (let i = lastDay.getDay(); i >= 0; i--) {
-  days += `<div class="prev">${31 - i}</div>`;
-}
-
-for (let i = 1; i <= 31; i++) {
+for (let i = 1; i <= lastDay; i++) {
   days += `<div>${i}</div>`;
 }
-
-for (let i = 1; i <= 6; i++) {
-  days += `<div>${i}</div>`;
-}
-
 monthDays.innerHTML = days;
